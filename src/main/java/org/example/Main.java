f9fe45b935ee704274ba8aafdea75dc429317d26
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-10-12 16:50:02 +0800

    Add async query

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import com.alibaba.graphscope.groot.sdk.BasicAuth;
 import com.alibaba.graphscope.groot.sdk.GrootClient;
+import com.alibaba.graphscope.proto.groot.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.apache.tinkerpop.gremlin.driver.*;
 
 import java.io.IOException;
+import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
@@ -30,88 +38,42 @@ public class Main {
         if (gremlinClient != null) {
             gremlinClient.close();
         }
-        System.out.println("yy");
     }
-    public void query() {
-        ResultSet results = gremlinClient.submit("g.V().limit(3).valueMap()");
+    public void query(String query) {
+        ResultSet results = gremlinClient.submit(query);
         for (Result result : results) {
             System.out.println(result.getObject());
         }
-        System.out.println("xx");
+    }
+
+    public void testQueryAsync() {
+        while (true) {
+            try {
+                queryAsync();
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                break;
+            }
+        }
+    }
+    public void queryAsync() throws InterruptedException {
+        try {
+            ResultSet results = gremlinClient.submitAsync("g.V().limit(1)").get();
+        } catch (ExecutionException ex) {
+            System.out.println(ex);
+        }
     }
 
     public void getSchema() {
         System.out.println(grootClient.getSchema());
     }
 
-//    public void createSchema() throws IOException {
-//        String path = "/Users/siyuan/CLionProjects/graphscope/interactive_engine/groot-client/src/test/resources/schema.json";
-//        String json = new String(Files.readAllBytes(Path.of(path)), StandardCharsets.UTF_8);
-//        grootClient.loadJsonSchema(json);
-//    }
-//
-//    private void testAddVerticesEdges() {
-//        for (int i = 0; i < 10; ++i) {
-//            Map<String, String> properties = new HashMap<>();
-//            properties.put("id", String.valueOf(i));
-//            properties.put("name", "person-" + i);
-//            properties.put("age", String.valueOf(i + 20));
-//            grootClient.addVertex("person", properties);
-//
-//            properties.clear();
-//            properties.put("id", String.valueOf(i));
-//            properties.put("name", "software-" + i);
-//            properties.put("lang", String.valueOf(i + 200));
-//            grootClient.addVertex("software", properties);
-//        }
-//
-//        for (int i = 0; i < 10; ++i) {
-//            Map<String, String> srcPk = new HashMap<>();
-//            Map<String, String> dstPk = new HashMap<>();
-//            Map<String, String> properties = new HashMap<>();
-//
-//            srcPk.put("id", String.valueOf(i));
-//            dstPk.put("id", String.valueOf(i));
-//            properties.put("weight", String.valueOf(i * 100));
-//            grootClient.addEdge("created", "person", "software", srcPk, dstPk, properties);
-//        }
-//        long snapshotId = grootClient.commit();
-//        grootClient.remoteFlush(snapshotId);
-//        System.out.println("Finished adding vertices and edges");
-//    }
-//
-//    private void testUpdateDeleteEdge() {
-//        Map<String, String> srcPk = new HashMap<>();
-//        Map<String, String> dstPk = new HashMap<>();
-//        Map<String, String> properties = new HashMap<>();
-//
-//        srcPk.put("id", String.valueOf(0));
-//        dstPk.put("id", String.valueOf(0));
-//        properties.put("weight", String.valueOf(10000));
-//        grootClient.updateEdge("created", "person", "software", srcPk, dstPk, properties);
-//        long snapshotId = grootClient.commit();
-//        grootClient.remoteFlush(snapshotId);
-//        System.out.println("Finished update edge person-0 -> software-0");
-//
-//        grootClient.deleteEdge("created", "person", "software", srcPk, dstPk);
-//        snapshotId = grootClient.commit();
-//        grootClient.remoteFlush(snapshotId);
-//        System.out.println("Finished delete edge person-0 -> software-0");
-//    }
-//
-//    private void testUpdateDeleteVertex() {
-//        Map<String, String> properties = new HashMap<>();
-//        properties.put("id", String.valueOf(0));
-//        properties.put("name", "marko-0-updated");
-//        grootClient.updateVertex("person", properties);
-//        grootClient.remoteFlush(snapshotId);
-//        System.out.println("Finished update vertex person-0");
-//
-//        Map<String, String> pk_properties = new HashMap<>();
-//        grootClient.deleteVertex("person", pk_properties);
-//        grootClient.remoteFlush(snapshotId);
-//        System.out.println("Finished delete vertex person-0");
-//    }
+    public void createSchema() throws IOException {
+        String path = "/Users/siyuan/CLionProjects/graphscope/interactive_engine/groot-client/src/test/resources/schema.json";
+        String json = new String(Files.readAllBytes(Path.of(path)), StandardCharsets.UTF_8);
+        grootClient.loadJsonSchema(json);
+    }
+
 
     public static void main(String[] args) throws IOException {
         String host = "47.57.139.88";
@@ -120,12 +82,11 @@ public class Main {
         Main main = new Main(host, grpcPort, gremlinPort);
         main.getSchema();
 //        main.createSchema();
-//        main.testAddVerticesEdges();
-//        main.testUpdateDeleteEdge();
-//        main.testUpdateDeleteVertex();
-//        main.query();
-//        main.close();
-        System.out.println("zz");
+        main.query("g.V().limit(3).valueMap()");
+        main.close();
+
         System.exit(0);
     }
+
+
 }
